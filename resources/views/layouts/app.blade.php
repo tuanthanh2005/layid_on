864380ed8384dd39762173b8fe3b8e6e434ed783
Thanh Tuấn <154106060+tuanthanh2005@users.noreply.github.com>
2026-03-19 01:37:40 +0700

    a
    This reverts commit 48cff1b574ba6d5559450a2486df07766dcf9576.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -174,8 +174,6 @@
         </div>
     </footer>
 
-    @livewire('components.floating-chatbot')
-
     <!-- Toast Container -->
     <div class="toast-container position-fixed bottom-0 end-0 p-3" style="z-index: 1100">
         @if(session('success'))
